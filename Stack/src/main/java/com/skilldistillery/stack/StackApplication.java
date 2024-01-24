b83a4cfa7c864aaf2f7f75518e0b2411ae45ea06
Parrisu <parris.a.jones@gmail.com>
2024-01-24 10:36:28 -0800

    add security options

--- Stack/src/main/java/com/skilldistillery/stack/StackApplication.java
+++ Stack/src/main/java/com/skilldistillery/stack/StackApplication.java
@@ -2,12 +2,20 @@ package com.skilldistillery.stack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class StackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StackApplication.class, args);
+	}
+	
+	@Bean
+	PasswordEncoder configurePasswordEncoder() {
+	   return new BCryptPasswordEncoder();
 	}
 
 }
