bda457d000635808177c8551344c1742d7f313f9
Max <maxtremaine@aol.com>
2024-01-26 15:25:52 -0700

    message class fixed fields for test

---
+++ JPAStack/src/test/java/com/skilldistillery/stack/entities/MessageTest.java
@@ -0,0 +1,57 @@
+package com.skilldistillery.stack.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class MessageTest {
+
+		private static EntityManagerFactory emf;
+		private EntityManager em;
+		private Message message;
+		
+		@BeforeAll
+		static void setUpBeforeClass() throws Exception {
+			emf = Persistence.createEntityManagerFactory("JPAStack");
+			
+		}
+
+		@AfterAll
+		static void tearDownAfterClass() throws Exception {
+			
+			emf.close();
+		}
+
+		@BeforeEach
+		void setUp() throws Exception {
+			em = emf.createEntityManager();
+			message = em.find(Message.class, 1);
+		}
+
+		@AfterEach
+		void tearDown() throws Exception {
+			em.close();
+			message = null;
+			
+		}
+
+		@Test
+		void test_Message_entity_mapping() {
+			assertNotNull(message);
+			assertEquals(1, message.getReceiver());
+			
+		}
+
+	}
+
+
+
