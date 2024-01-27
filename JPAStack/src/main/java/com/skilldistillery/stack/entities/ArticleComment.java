bed6e1cd918e16d3984ebc7f53ecd825defd1e76
Parrisu <parris.a.jones@gmail.com>
2024-01-26 16:38:56 -0800

    complete articleComment test

---
+++ JPAStack/src/main/java/com/skilldistillery/stack/entities/ArticleComment.java
@@ -0,0 +1,110 @@
+package com.skilldistillery.stack.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="article_comment")
+public class ArticleComment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+	
+	@Column(name="created_on")
+	private LocalDateTime created;
+	
+	@Column(name="updated_on")
+	private LocalDateTime updated;
+	
+	@ManyToOne
+	@JoinColumn(name="article_id")
+	private Article article;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	@ManyToOne (fetch = FetchType.EAGER)
+	@JoinColumn(name="reply_id")
+	private ArticleComment reply;
+	
+	public ArticleComment() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public Article getArticle() {
+		return article;
+	}
+
+	public void setArticle(Article article) {
+		this.article = article;
+	}
+
+	public ArticleComment getReply() {
+		return reply;
+	}
+
+	public void setReply(ArticleComment reply) {
+		this.reply = reply;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "Article [id=" + id + ", content=" + content + ", created=" + created + ", updated=" + updated + "]";
+	}
+	
+	
+	
+
+}
