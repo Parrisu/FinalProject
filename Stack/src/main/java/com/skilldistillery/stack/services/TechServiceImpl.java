3d606e4a49cb4f845e764fde0de0065fd5d35631
Max <maxtremaine@aol.com>
2024-01-29 10:48:52 -0700

    Added Tech Controller and mapping to show technology

---
+++ Stack/src/main/java/com/skilldistillery/stack/services/TechServiceImpl.java
@@ -0,0 +1,23 @@
+package com.skilldistillery.stack.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skilldistillery.stack.entities.Technology;
+import com.skilldistillery.stack.repositories.TechnologyRepository;
+
+public class TechServiceImpl implements TechService {
+	
+	@Autowired
+	private TechnologyRepository techRepo;
+	
+
+
+
+	@Override
+	public List<Technology> showAllTech() {
+		return techRepo.findAll();
+	}
+
+}
