f7dcf7918771e98dc4f20259ff7dd0a09b95fb1b
Parrisu <parris.a.jones@gmail.com>
2024-01-30 14:20:24 -0800

    working add / remove stack technologies from user profile

--- ngStack/src/app/services/user.service.ts
+++ ngStack/src/app/services/user.service.ts
@@ -5,6 +5,7 @@ import { Observable, catchError, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
 import { User } from '../models/user';
+import { Technology } from '../models/technology';
 
 @Injectable({
   providedIn: 'root',
@@ -111,4 +112,36 @@ export class UserService {
       })
     );
   }
+
+  addOrRemoveTech(id: number, tech: Technology){
+    const endpoint = `${this.baseUrl}/account/${id}`;
+    const credentials = this.auth.getCredentials();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Basic ${credentials}`,
+        'X-Requested-With': 'XMLHttpRequest',
+      }),
+    };
+
+    return this.http.post<User>(endpoint, tech, httpOptions).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error(`
+                UserService.updateUser(user: User);
+                Error while attempting POST to ${endpoint}.
+                With body ${JSON.stringify(tech)}
+              `)
+        );
+      })
+    );
+  }
+
+
+
+
+
+
+
 }
