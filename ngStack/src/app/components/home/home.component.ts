ac497b5aa260c6cd721eb4612f174d41bd21d8d5
Jacob Snell <jake.r.snell@gmail.com>
2024-01-31 23:26:06 -0700

    Update home

--- ngStack/src/app/components/home/home.component.ts
+++ ngStack/src/app/components/home/home.component.ts
@@ -12,8 +12,9 @@ import { Nodes } from '../../models/node';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   nodeList: Nodes[] = [];
+  nodeNodeList: Nodes[][] = [];
 
   constructor(private nodeService: NodeService, private router: Router) {}
   ngOnInit(): void {
@@ -24,10 +25,24 @@ export class HomeComponent implements OnInit{
     this.nodeService.showAll().subscribe({
       next: (nodeList) => {
         this.nodeList = nodeList;
+        this.nodeNodeList = this.subdivideNodes(nodeList);
       },
       error: (failed) => {
         console.error('nodeService.showall: error getting nodes');
       },
     });
+  }
+
+  subdivideNodes(list: Nodes[]): Nodes[][] {
+    let full: Nodes[][] = [];
+    let temp: Nodes[] = [];
+    for (let i = 0; i < list.length; i++) {
+      if (i % 3 == 0 && i !== 0) {
+        temp = [];
+        full.push(temp);
+      }
+      temp.push(list[i]);
+    }
+    return full;
   }
 }
